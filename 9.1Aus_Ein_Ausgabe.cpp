a4373033c6dd3125bbc4164f8e62a14ffb25d6c7
Wicker1090 <160037423+Wicker1090@users.noreply.github.com>
2024-02-14 16:45:04 +0100

    ok

--- 9.1Aus_Ein_Ausgabe.cpp
+++ 9.1Aus_Ein_Ausgabe.cpp
@@ -1,21 +1,24 @@
-#include <cstdlib>
+//
 #include <iostream>  //
 #include <fstream>  //Datei beschreiben und lesen
 #include <string>
+#include <vector>
 
-using namespace std;                
-int main(int argc, char *argv[])        //
-{
-    char n_t, n_f;
+using namespace std;
+
+
+int main() {
     ifstream infile("Hello_World.txt");
-    cout << "Terminaleingabe: "; cin >> n_t;
+    cout << "Terminaleingabe: "; cin >>;
     cout << "Dateieingabe: " << endl;
-    infile >> n_f;
     
     cout << endl;
     cout << " Die Dateiausgabe lautet." << n_f << endl;
     cout << " Die Terminaleingabe lautet. " << n_t << endl;
     cout << endl;
+
+    ofstream output_file("/example5.txt")
     
     return EXIT_SUCCESS;
+    //ENDE
 }
